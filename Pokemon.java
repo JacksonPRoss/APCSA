13ba903a2e3cd1b477bd3cafea34b988ab1c74f6
Jackson Paul Ross <20017314@HartlandSchools.us>
2019-08-26 07:48:24 -0400

    Pokemon Practice Assignment

---
+++ Pokemon.java
@@ -0,0 +1,10 @@
+public class Pokemon{
+public static void main(String[] args) {
+System.out.println("Pikachu welcomes you to the world of Pokemon");
+System.out.println(" (\\___/)");
+System.out.println(" (o^.^)");
+System.out.println("z(_(\")(\")");
+
+
+}
+}
