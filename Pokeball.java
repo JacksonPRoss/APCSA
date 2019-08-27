70880dee02435fb9128dee52cb107bb94984345a
Jackson Paul Ross <20017314@HartlandSchools.us>
2019-08-27 08:02:50 -0400

    Pokeball

---
+++ Pokeball.java
@@ -0,0 +1,14 @@
+public class Pokeball{
+public static void main(String[] args) {
+System.out.println("   @@@@@@@@@");
+System.out.println("  @@@@@@@@@@@");
+System.out.println(" @@@@@@@@@@@@@");
+System.out.println("@@@@@@@@@@@@@@@");
+System.out.println("||||| [\"\"\"\"]||||");
+System.out.println("||||| [...]|||||");
+System.out.println("0000000000000000");
+System.out.println(" 00000000000000");
+System.out.println("  000000000000");
+System.out.println("   0000000000");
+}
+}
