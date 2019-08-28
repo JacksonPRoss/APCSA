c650c51854876360cfc40c30ba20a64a0dc53c5c
Jackson Paul Ross <20017314@HartlandSchools.us>
2019-08-28 08:06:58 -0400

    s

---
+++ FightSong.java
@@ -0,0 +1,24 @@
+public class FightSong{
+public static void main(String[] args) {
+goTeam();
+System.out.println("");
+goTeam();
+west();
+goTeam();
+System.out.println("");
+goTeam();
+west();
+goTeam();
+System.out.println("");
+goTeam();
+}
+public static void goTeam(){
+System.out.println("Go, team, go!");
+System.out.println("You can do it.");
+}
+public static void west(){
+System.out.println("You're the best,");
+System.out.println("in the West.");
+}
+}
+
---
+++ Shining.java
@@ -0,0 +1,45 @@
+public class Shining{
+public static void main(String[] args) {
+Jack4();
+}
+public static void Jack(){
+System.out.println("All work and no play makes Jack a dull boy");
+}
+public static void Jack2(){
+Jack();
+Jack();
+Jack();
+Jack();
+Jack();
+Jack();
+Jack();
+Jack();
+Jack();
+Jack();
+Jack();
+}
+public static void Jack3(){
+Jack2();
+Jack2();
+Jack2();
+Jack2();
+Jack2();
+Jack2();
+Jack2();
+Jack2();
+Jack2();
+Jack2();
+}
+public static void Jack4(){
+Jack3();
+Jack3();
+Jack3();
+Jack3();
+Jack3();
+Jack3();
+Jack3();
+Jack3();
+Jack3();
+Jack3();
+}
+}
