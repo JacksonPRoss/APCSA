342efbef6a00a5ac6a3c9d5ac04c86a82ca7629c
JacksonPRoss <54401672+JacksonPRoss@users.noreply.github.com>
2019-09-06 04:12:34 -0400

    Add files via upload

---
+++ OldLady.java
@@ -0,0 +1,73 @@
+// Jack Ross Unit 1 Project 3
+
+public class OldLady{
+   public static void main(String[]args){
+      Swallowed();
+      System.out.println("fly");
+      Perhaps();
+      System.out.println("");
+      Swallowed();
+      System.out.println("spider");
+      System.out.println("That wringled and jinled inside her");
+      Catch();
+      Perhaps();
+      System.out.println();
+      Swallowed();
+      System.out.println("bird");
+      System.out.println("How absurd to swallow a bird");
+      Catch2();
+      Catch();
+      Perhaps();
+      System.out.println();
+      Swallowed();
+      System.out.println("cat");
+      System.out.println("Imagine that swallow a cat");
+      Catch3();
+      Catch2();
+      Catch();
+      Perhaps();
+      Swallowed();
+      System.out.println("dog");
+      System.out.println("What a hog to swallow a dog");
+      Catch4();
+      Catch3();
+      Catch2();
+      Catch();
+      Perhaps();
+      System.out.println();
+      Swallowed();
+      System.out.println("Horse");
+      System.out.print("She died of course.");
+      
+
+}
+      public static void Swallowed() {
+      System.out.print("There was a old lady who swallowed a ");
+      }
+      public static void Perhaps() {
+      System.out.println("I don't know why she swallowed that fly");
+      System.out.println("Perhaps she'll die.");
+      }
+  
+      public static void Catch() {
+      System.out.println("She swallowed the spider to catch the fly,");
+      }
+      
+      public static void Catch2() {
+      System.out.println("She swallowed the bird to catch the spider,");
+
+  
+      }
+      public static void Catch3() {
+      System.out.println("She swallowed the cat to catch the bird,");
+      }
+      
+      public static void Catch4() {
+      System.out.println("She swallowed the dog to catch the cat,");
+
+      
+      
+      }
+      }
+
+
